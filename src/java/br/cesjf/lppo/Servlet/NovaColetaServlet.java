e1b50d4cb16c7194d08cf96cf029f598b1f059ab
BrunahAlves <Brunahalves94@gmail.com>
2017-05-22 00:13:21 -0300

    Cria NovaColetaServlet e nova-coleta

---
+++ src/java/br/cesjf/lppo/Servlet/NovaColetaServlet.java
@@ -0,0 +1,50 @@
+package br.cesjf.lppo.Servlet;
+
+import br.cesjf.lppo.Coleta;
+import br.cesjf.lppo.DAO.ColetaDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Timestamp;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Bruna Alves
+ */
+@WebServlet(name = "NovaColetaServlet", urlPatterns = {"/NovaColeta.html"})
+public class NovaColetaServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.getRequestDispatcher("WEB-INF/nova-coleta.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Coleta novaColeta = new Coleta();
+
+        novaColeta.setDescricao(request.getParameter("descricao"));
+        novaColeta.setData(Timestamp.valueOf(request.getParameter("data")));
+
+        try {
+            ColetaDAO dao = new ColetaDAO();
+            dao.cria(novaColeta);
+        } catch (Exception ex) {
+            request.setAttribute("mensagem", ex);
+            request.getRequestDispatcher("WEB-INF/nova-coleta.jsp").forward(request, response);
+            return;
+        }
+
+        response.sendRedirect("NovaLeitura.html");
+
+    }
+
+}
---
+++ web/WEB-INF/nova-coleta.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : planejamento
+    Created on : 21/05/2017, 23:31:33
+    Author     : Bruna Alves
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Planejamento</title>
+    </head>
+    <body>
+        <h1>Fase de planejamento</h1>
+         <div style="color: red;">${mensagem}</div>
+        <form method="post">
+            <div><label> Descrição: <input type="text" name="descricao" placeholder="Digite a descricao" /></label></div>
+            <div><label> Data: <input type="text" name="data" placeholder="Digite a data" /></label></div>
+            <div><input type="submit" /></div>   
+        </form>
+    </body>
+</html>
