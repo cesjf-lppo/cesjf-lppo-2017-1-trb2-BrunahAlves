5c635e0baef6e84c63750d96927c34fc61053e7a
BrunahAlves <Brunahalves94@gmail.com>
2017-05-22 00:12:57 -0300

    Cria ColetaDAO e ConnectionFactory

---
+++ src/java/br/cesjf/lppo/DAO/ColetaDAO.java
@@ -0,0 +1,32 @@
+package br.cesjf.lppo.DAO;
+
+import br.cesjf.lppo.Coleta;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ColetaDAO {
+
+    private final PreparedStatement opNovaColeta;
+
+    public ColetaDAO() throws Exception {
+
+        Connection conexao = ConnectionFactory.createConnection();
+        opNovaColeta = conexao.prepareStatement("INSERT INTO Coleta(descricao, data) VALUES(?,?)");
+
+    }
+
+    public void cria(Coleta novaColeta) throws Exception {
+        try {
+            opNovaColeta.clearParameters();
+            opNovaColeta.setString(1, novaColeta.getDescricao());
+            opNovaColeta.setDate(2, (Date) novaColeta.getData());
+            opNovaColeta.executeUpdate();
+        } catch (SQLException ex) {
+            throw new Exception("Erro ao inserir novo Pedido!", ex);
+        }
+    }
+}
