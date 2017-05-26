85403a8c9dd343ba08a4d8bc22feefc4cc2be9b3
BrunahAlves <Brunahalves94@gmail.com>
2017-05-26 07:14:59 -0300

    Edita ColetaDAO para listar por coleta

--- src/java/br/cesjf/lppo/DAO/ColetaDAO.java
+++ src/java/br/cesjf/lppo/DAO/ColetaDAO.java
@@ -4,18 +4,23 @@ import br.cesjf.lppo.Coleta;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ColetaDAO {
 
     private final PreparedStatement opNovaColeta;
+    private final PreparedStatement opListarColeta;
 
     public ColetaDAO() throws Exception {
 
         Connection conexao = ConnectionFactory.createConnection();
         opNovaColeta = conexao.prepareStatement("INSERT INTO Coleta(descricao, dat) VALUES(?,?)");
+        opListarColeta = conexao.prepareStatement("SELECT * FROM Coleta GROUP BY coleta");
 
     }
 
@@ -29,5 +34,28 @@ public class ColetaDAO {
         } catch (SQLException ex) {
             throw new Exception("Erro ao inserir nova Coleta!", ex);
         }
+    }
+
+    public List<Coleta> listByColeta() throws Exception {
+        try {
+            List<Coleta> pedidos = new ArrayList<>();
+
+            ResultSet resultado = opListarColeta.executeQuery();
+
+            while (resultado.next()) {
+                Coleta novaColeta = new Coleta();
+
+                novaColeta.setId(resultado.getLong("id"));
+                novaColeta.setDescricao(resultado.getString("descricao"));
+                //Adicionar o setData
+                //Coleta.add(novaColeta);
+            }
+
+            return pedidos;
+
+        } catch (SQLException ex) {
+            throw new Exception("Erro ao listar os pedidos no banco!", ex);
+        }
+    
     }
 }
